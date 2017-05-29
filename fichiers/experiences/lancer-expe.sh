3fa61328d7f3ae424731d6fcb32ddcce679dd983
Paul Gaulier <pgaulier@emi.u-bordeaux1.fr>
2017-05-29 17:09:28 +0200

    pdf

--- fichiers/experiences/lancer-expe.sh
+++ fichiers/experiences/lancer-expe.sh
@@ -8,7 +8,7 @@ THREADS=$(seq 2 2 24) # nombre de threads
 PARAM="-n -s 256 -a -i 1000" # parametres commun à toutes les executions 
 
 execute (){
-EXE="../prog $* $PARAM"
+EXE="./prog $* $PARAM"
 OUTPUT="$(echo $EXE | tr -d ' ')"
 for nb in $ITE; do for OMP_NUM_THREADS in $THREADS; do  echo -n "$OMP_NUM_THREADS " >> $OUTPUT ; $EXE 2>> $OUTPUT; done; done
 }
